7c890e6a347b9c2ee738194105dc12abcaedc5b2
Jashwanth001 <jashwanthskt@gmail.com>
2024-08-06 15:58:11 +0530

    Modify New

---
+++ script.js
@@ -0,0 +1,174 @@
+// let price = 100;
+// let tax = 500;
+// let product = 'Apple';
+// let total = price + tax;
+// console.log(product)
+// console.log("The Price is "+total);
+
+//const price = document.getElementById("price");
+//document.write("The Price is "+price);
+
+// let Fruit_Name = "Mango";
+// let Count = 5;
+// let price = 70;
+// let total = Count*price;
+// console.log(Fruit_Name);
+// console.log("Total Price is "+total); 
+
+// function iphone(){
+//     console.log("Iphone 13 Pro Max is available");
+// }
+// iphone()
+
+// function redmi(){
+//     console.log("Redmi is available");
+// }
+// redmi()
+
+// let a = 10
+// let b = 10
+
+// function add(){
+//     console.log("Add:"+(a+b))
+// }
+// add()
+
+// let factor = "Vijay"
+// let fplayer = 'Rohit'
+// let fmovie = 'Theeran'
+// function favourite(){
+//     console.log("Favourite Actor:"+factor)
+//     console.log("Favourite player:"+fplayer)
+//     console.log("Favourite movie:"+fmovie)
+// }
+// favourite()
+
+// function area(length,breath){
+//     console.log(length*breath)
+// }
+
+// area(20,30)
+
+
+// let a = 10
+// let b = 10
+// function add(a,b){
+//     return a+b
+// }
+// var a = add(5,5)
+// console.log(a)
+
+// let raining = 'yes'
+// if(yes){
+//     console.log("Take an umbrella")
+// }
+// else{
+//     console.log("Enjoy the sunshine")
+// }
+
+// let rain = true
+
+// if(true){
+//     console.log("Rainly")
+// }
+// else{
+//     console.log("enjoy the rain")
+// }
+
+
+// let work = true
+
+// if(true){
+//     console.log("Great Job")
+// }
+// else{
+//     console.log("Finish your work")
+// }
+
+// let color = 'red'
+// // let color2 = yellow
+// // let color3 = green
+// if(color == 'red'){
+//     console.log("Stop")
+// }
+// else if(color == 'yellow'){
+//     console.log("Get Ready")
+// }
+// else{
+//     console.log("Go")
+// }
+
+// let season = 'autumn'
+
+// if(season == 'spring'){
+//     console.log("Enjoy the blooming")
+// }
+
+// if(season == 'summer'){
+//     console.log("Have fun")
+// }
+
+// if(season == 'autumn'){
+//     console.log("Admire the colorfull")
+// }
+
+// if(season == 'winter'){
+//     console.log("Bundle up and stay warm")
+// }
+
+// let score = 90
+
+// // if condition
+
+// if(score<=50){
+//     console.log("You Need to improve")
+// }
+
+// else if(score >=50 && score<=70){
+//     console.log("Good Job")
+// }
+
+// else if(score>=70){
+//     console.log("Excellent Performance")
+// }
+// else{
+//     console.log("Invalid Score")
+// }
+
+// let number = 10;
+
+// if(number%2==0){
+//     console.log("Even")
+// }
+// else{
+//     console.log("Odd")
+// }
+
+// let character = 'a'
+
+// if (character== 'a' && 'e' && 'i' && 'o' && 'u'){
+//     console.log("Vowel")
+// }
+// else{
+//     console.log("Consonant")
+// }
+
+// for( count = 0; count<=10; count=count+2){
+//     console.log(count)
+// }
+
+// for( count = 1; count<=10; count=count+1){
+//     if(count%2==0){
+//         console.log(count)
+//     }
+// }
+
+// for (i = 0;i<=10; i++){
+//     count = i  + " X " + "2" + "=" +i*2;
+//     console.log(count)
+// }
+
+        // Random Number Generator
+        
+        var a = Math.random();
+        console.log(Math.floor(a*10)+1);
